7c1e26cfcc4cf9745f0da0dc1b5af173711f9002
Perry Alex <ifechinwokedi@gmail.com>
2026-01-17 02:22:46 +0100

    Remove mobile menu image and update header component for mobile menu toggle

--- components/Header.vue
+++ components/Header.vue
@@ -30,12 +30,12 @@ onUnmounted(() => {
       atTop ? 'bg-transparent' : 'bg-[#42389E] ',
     ]"
   >
-    <nav class="w-full flex items-center justify-between px-4  md:px-29 h-full">
+    <nav class="w-full flex items-center justify-between px-4 md:px-29 h-full">
       <!-- Logo -->
       <div class="flex shrink-0">
         <NuxtLink to="/" class="text-xl font-bold text-white">
           <img
-            class="lg:w-[205.5px] sm:w-[205.5px] w-20"
+            class="lg:w-[205.5px] sm:w-[205.5px] w-25"
             src="/images/logo.png"
             alt="Logo"
           />
@@ -80,7 +80,7 @@ onUnmounted(() => {
       <div class="hidden md:flex items-center">
         <a
           href="#"
-          class="bg-[#F9A71E] xl:w-[214px] xl:h-[53px] lg:w-[186px] lg:h-[47px] md:h-[41px] w-[210px] h-[50px] rounded-md font-semibold text-[#292462] xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[20px]  text-[24px] grid place-content-center font-['DM Sans']"
+          class="bg-[#F9A71E] xl:w-[214px] xl:h-[53px] lg:w-[186px] lg:h-[47px] md:h-[41px] w-[210px] h-[50px] rounded-md font-semibold text-[#292462] xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[20px] text-[24px] grid place-content-center font-['DM Sans']"
         >
           Start Sandbox
         </a>
@@ -90,10 +90,20 @@ onUnmounted(() => {
       <div class="md:hidden">
         <button
           type="button"
-          class="text-white hover:text-gray-900"
+          class="text-white"
           @click="toggleMobile"
+          :aria-expanded="mobileOpen"
+          aria-controls="mobile-menu"
         >
-          <img src="@/app/assets/images/mobile-menu.png" />
+          <img
+            class="w-7"
+            :src="
+              mobileOpen
+                ? '/images/close-menu.png'
+                : '/images/mobile-menu.png'
+            "
+            :alt="mobileOpen ? 'Close menu' : 'Open menu'"
+          />
         </button>
       </div>
     </nav>
